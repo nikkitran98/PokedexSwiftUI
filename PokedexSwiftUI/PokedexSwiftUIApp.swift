8f606e7c65779ac20fa773711b0181593f5a9937
Nikki Tran <nikkitran7898@gmail.com>
2023-06-01 12:18:04 -0400

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by Nikki Tran on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
